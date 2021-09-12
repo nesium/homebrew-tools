6294be84aaa6ad8a535b8baa77e4034d021f35b0
mb <mb@nesium.com>
2021-09-12 13:44:36 +0100

    added new release

--- xcode-color-assets.rb
+++ xcode-color-assets.rb
@@ -1,8 +1,8 @@
 class XcodeColorAssets < Formula
   desc "Create (dark mode compatible) color assets for Xcode programmatically from a CSS-like textfile"
   homepage "https://github.com/nesium/xcode-color-assets"
-  url "https://github.com/nesium/xcode-color-assets/releases/download/0.5.0/xcode-color-assets-0.5.0.tar.gz"
-  sha256 "814d22571d1093bf6bb2dd67dd921d0bfb1ccefbe8a90c81c796cdda8038b85b"
+  url "https://github.com/nesium/xcode-color-assets/releases/download/0.6.0/xcode-color-assets-0.6.0.tar.gz"
+  sha256 "e96aa9297e9499368c0c1d8f950e9040b2e340ff26d3e329a99c0a04b6eb0753"
 
   bottle :unneeded
 
