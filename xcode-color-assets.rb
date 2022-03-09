b98cedd32cb7e62128a1a015b4bb09a8f57bc2f8
Marc Bauer <mb@nesium.com>
2022-03-09 10:20:40 +0100

    Remove :bottle unneeded

--- xcode-color-assets.rb
+++ xcode-color-assets.rb
@@ -4,8 +4,6 @@ class XcodeColorAssets < Formula
   url "https://github.com/nesium/xcode-color-assets/releases/download/0.6.0/xcode-color-assets-0.6.0.tar.gz"
   sha256 "e96aa9297e9499368c0c1d8f950e9040b2e340ff26d3e329a99c0a04b6eb0753"
 
-  bottle :unneeded
-
   def install
     bin.install "xcode-color-assets"
   end
